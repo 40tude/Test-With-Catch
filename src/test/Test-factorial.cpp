12c2b92eb2366b6a915c8be4d21179b37a3e3b51
Philippe BAUCOUR <philippe.baucour@gmail.com>
2017-03-01 11:58:01 +0100

    Encore des modis dans les commentaires (pour jouer avec Git)

--- src/test/Test-factorial.cpp
+++ src/test/Test-factorial.cpp
@@ -6,6 +6,6 @@
 TEST_CASE("Factorials are computed", "[factorial]") {
 	REQUIRE(Factorial(1) == 1);
 	REQUIRE(Factorial(2) == 2);
-	REQUIRE(Factorial(3) == 12);												// retour dans msvc si -p en argument et si lancé depuis msvc
+	REQUIRE(Factorial(3) == 12);							// retour dans msvc si on passe -p en argument et si le testeur est lancé depuis msvc
 	REQUIRE(Factorial(10) == 3628800);
 }
